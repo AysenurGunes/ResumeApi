af58daed8fbad1899ebd358f60a40ddb8fc5dcd6
Ayşenur Güneş <gunesaysenur94@outlook.com>
2023-02-08 21:45:46 +0300

    Employeer model added

--- ResumeApi/Models/Employeer.cs
+++ ResumeApi/Models/Employeer.cs
@@ -1,6 +1,13 @@
-﻿namespace ResumeApi.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ResumeApi.Models
 {
     public class Employeer
     {
+        public int EmployeerID { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        
     }
 }
