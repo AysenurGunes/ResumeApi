ba5a23e34fc2fd96df380a9e028c377102a35a45
Ayşenur Güneş <gunesaysenur94@outlook.com>
2023-02-08 22:47:37 +0300

    controllers added

---
+++ ResumeApi/Controllers/EmployeerController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ResumeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeerController : ControllerBase
+    {
+        // GET: api/<EmployeerController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EmployeerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EmployeerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EmployeerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EmployeerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
