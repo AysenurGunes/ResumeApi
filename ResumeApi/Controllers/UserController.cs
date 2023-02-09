763b9ac6bb4291641fd5b3d32956943a913338c9
AysenurGunes <gunesaysenur94@gmail.com>
2023-02-09 17:12:05 +0300

    user get services editted

--- ResumeApi/Controllers/UserController.cs
+++ ResumeApi/Controllers/UserController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ResumeApi.Helper;
+using ResumeApi.Models;
+using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,126 @@ namespace ResumeApi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        // GET: api/<UserController>
+        public List<User> users = new List<User>()
+        {
+            new User()
+            {
+                UserID= 1,
+                Email="abc@gmail.com",
+                FirstInformation= "ilk izlenim yazisi",
+                Name="Aysenur",
+                Surname="Gunes",
+                Password="123"
+            } ,
+            new User()
+            {
+                UserID= 2,
+                Email="abc@abc.com",
+                FirstInformation= "Abc yazilim firmasi",
+                Name="Abc",
+                Surname="Company",
+                Password="12"
+            } ,
+        };
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ServiceResponse<List<User>> Get()
         {
-            return new string[] { "value1", "value2" };
+            ServiceResponse<List<User>> response = new ServiceResponse<List<User>>();
+            try
+            {
+
+                response.Data = users;
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.ToString();
+                return response;
+            }
         }
 
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+
+        [HttpGet]
+        public ServiceResponse<User> Get([FromQuery] int id)
         {
-            return "value";
+            ServiceResponse<User> response = new ServiceResponse<User>();
+            try
+            {
+                response.Data = users.Where(c => c.UserID == id).FirstOrDefault();
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.ToString();
+                return response;
+            }
         }
 
-        // POST api/<UserController>
+
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ServiceResponse<User> Post([FromBody] User user)
         {
+            ServiceResponse<User> response = new ServiceResponse<User>();
+            try
+            {
+                users.Add(user);
+                response.Data = user;
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.ToString();
+                return response;
+            }
         }
 
-        // PUT api/<UserController>/5
+
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ServiceResponse<User> Put(int id, [FromBody] User user)
         {
+            ServiceResponse<User> response = new ServiceResponse<User>();
+            try
+            {
+                User user1 = users.Where(c => c.UserID == id).FirstOrDefault();
+                user1.Email = user.Email;
+                user1.Surname = user.Surname;
+                user1.Name = user.Name;
+                user1.FirstInformation = user.FirstInformation;
+                user1.Password = user.Password;
+
+                response.Data = user;
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.ToString();
+                return response;
+            }
         }
 
-        // DELETE api/<UserController>/5
+
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ServiceResponse<ActionResult> Delete(int id)
         {
+            ServiceResponse<ActionResult> response = new ServiceResponse<ActionResult>();
+            try
+            {
+                users.RemoveAt(c => c.UserID == id);
+
+                response.Data = Ok();
+                response.Success = true;
+                return response;
+            }
+            catch (Exception ex)
+            {
+                response.Error = ex.ToString();
+                return response;
+            }
         }
     }
 }
---
+++ ResumeApi/Helper/ServiceResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace ResumeApi.Helper
+{
+    public class ServiceResponse<TEntity>
+    {
+        public ServiceResponse()
+        {
+            
+        }
+        public ServiceResponse(TEntity data)
+        {
+            Data = data;
+        }
+
+        public ServiceResponse(string error)
+        {
+            Error = error;
+            Success = false;
+        }
+
+        public TEntity? Data { get; set; }
+        public bool Success { get; set; }
+        public string Error { get; set; }
+    }
+}
